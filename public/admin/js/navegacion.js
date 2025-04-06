f05616c6c327aa86f49d51386f881d8560d7588f
Jela <jela3105@gmail.com>
2025-04-05 20:43:58 -0600

    Update admin left navigation

---
+++ public/admin/js/navegacion.js
@@ -0,0 +1,23 @@
+function index() {
+    window.location = `./`;
+}
+
+function ProfesorAdministrar() {
+    window.location = `./profesores`;
+}
+
+function GrupoAdministrar() {
+    window.location = `./grupos`;
+}
+
+function AlumnoAdministrar() {
+    window.location = `./alumnos`;
+}
+
+function PasswordAdministrador() {
+    window.location = `./password`;
+}
+
+function SoporteAdministrador() {
+    window.location = `./soporte`;
+}
--- routes/admin.js
+++ routes/admin.js
@@ -6,12 +6,32 @@ const { requiereLogin, soloRol } = require('../auth');
 router.use(requiereLogin, soloRol(3));
 
 router.get('/', async (req, res) => {
+    res.render('admin/index', { user: req.session.user });
+});
+
+router.get('/profesores', async (req, res) => {
     const teachersRes = await fetch('http://localhost:3000/admin/teachers', {
         headers: { Authorization: `Bearer ${req.session.token}` }
     });
 
     const teachers = await teachersRes.json();
-    res.render('admin', { user: req.session.user, teachers });
+    res.render('admin/profesoresAdministrar', { user: req.session.user, teachers });
+});
+
+router.get('/grupos', async (req, res) => {
+    res.render('admin/gruposAdministrar', { user: req.session.user });
+});
+
+router.get('/alumnos', async (req, res) => {
+    res.render('admin/alumnosAdministrar', { user: req.session.user });
+});
+
+router.get('/password', async (req, res) => {
+    res.render('admin/passwordAdmin', { user: req.session.user });
+});
+
+router.get('/soporte', async (req, res) => {
+    res.render('admin/soporteAdmin', { user: req.session.user });
 });
 
 module.exports = router;
