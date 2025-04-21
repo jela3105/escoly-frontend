3765a1f41bf5d42f0095fa5e034d85c32931dbe5
Jela <jela3105@gmail.com>
2025-04-21 13:26:45 -0600

    Add support page

--- routes/tutor.js
+++ routes/tutor.js
@@ -4,8 +4,19 @@ const { requiereLogin, soloRol } = require('../auth');
 
 router.use(requiereLogin, soloRol(1));
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     res.render('tutor/index', { user: req.session.user });
+});
+
+router.get('/password', (req, res) => {
+    res.render('tutor/password', {
+        user: req.session.user,
+        formData: { currentPassword: '', newPassword: '', confirmNewPassword: '' }
+    });
+});
+
+router.get('/soporte', (req, res) => {
+    res.render('tutor/soporte', { user: req.session.user });
 });
 
 module.exports = router;
